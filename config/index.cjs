d92fc96831205c0a075dd0af54e3ecaf82a8ab9f
HuangJX0109 <138108447+HuangJX0109@users.noreply.github.com>
2023-06-29 17:41:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,7 +63,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oYgNi6ftp-XClzlGzpN4zGbRO57M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'gp4rc5R3-TDy_ifXUvZG4Pvarlc1kSH9la0Ztb5Osy0',
+      useTemplateId: '	PUkGe_doNTkpoPutjR4T07jXKFWndO8VvPYGUA6VxF0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-30',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
