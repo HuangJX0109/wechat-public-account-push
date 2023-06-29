a03bc676540a5175b0a4a750eb523d21e4490d5c
HuangJX0109 <138108447+HuangJX0109@users.noreply.github.com>
2023-06-29 18:08:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -61,7 +61,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小峰',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oYgNi6ftp-XClzlGzpN4zGbRO57M',
+      id: 'oYgNi6aPfWpSRGlvUMOiLjgMpko4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'r9lXpjWRJDlIn_Mc8aX-QM6MmBwVFH6d90byQsT8QVs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
